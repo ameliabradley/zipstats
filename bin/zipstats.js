fd84e36bb66dec937226282da555dcdf8057c56d
Lee Bradley <elephanthunter@gmail.com>
2020-07-04 23:44:05 -0500

    ✨ Initial commit

---
+++ bin/zipstats.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const zipstats = require('../index.js');
+
+if (process.argv.length != 3) {
+    console.log('zipstats cli stat display')
+    console.log('Usage: zipstats [FILENAME]')
+    return;
+}
+
+let [_0, _1, filename] = process.argv;
+
+if (!fs.existsSync(filename)) {
+    console.error(`File '${filename}' does not exist`);
+    return;
+}
+
+console.log(zipstats(filename));
